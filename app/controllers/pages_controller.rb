536646a13c008b2ebcee8a4f25e7c011a8eb4a8b
erdgeist <erdgeist@erdgeist.org>
2026-06-26 14:22:33 +0200

    Fix RSS feeds, xmlparser initializer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,21 +3,19 @@ class PagesController < ApplicationController
   # Private
   
   before_action :login_required
-  
+
   def preview
     @page = Page.find(params[:id])
-    
+
     if @page
       template = @page.valid_template
       render(
-        :file => template,
-        :layout => "application"
+        template: template,
+        layout: "application"
       )
     end
-    
   end
-  
-  
+
   def sort_images
     page = Page.find(params[:id])
     page.update_assets(params[:images])
--- config/initializers/xmlparser.rb
+++ config/initializers/xmlparser.rb
@@ -1,10 +1,15 @@
 class XML::Node
-  def replace_with(other) 
+  def replace_with(other)
     self.next = other
     remove!
   end
 end
 
+# Builder 3.x escapes content by default. Override _escape to pass text
+# through raw, preserving existing behaviour from the Rails 2 era.
+# Note: require builder first to ensure XmlBase < BasicObject is already
+# defined before we reopen it.
+require 'builder'
 module Builder
   class XmlBase
     def _escape(text)
