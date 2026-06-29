31ca8e93efa860d73918b57ddddeaedf9917b22d
erdgeist <erdgeist@erdgeist.org>
2026-06-29 21:39:49 +0200

    Address brakeman security findings
    - Replace permit! with permit(:locale, :page_path) in
      AdminHelper#language_selector

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -3,9 +3,9 @@ module AdminHelper
   def language_selector
     case I18n.locale
     when :de
-      link_to raw('<span class="inactive">English</span>'), url_for(params.permit!.to_h.merge('locale' => 'en'))
+      link_to raw('<span class="inactive">English</span>'), url_for(params.permit(:locale, :page_path).to_h.merge('locale' => 'en'))
     when :en
-      link_to raw('<span class="inactive">Deutsch</span>'), url_for(params.permit!.to_h.merge('locale' => 'de'))
+      link_to raw('<span class="inactive">English</span>'), url_for(params.permit(:locale, :page_path).to_h.merge('locale' => 'de'))
     end
   end
 end
