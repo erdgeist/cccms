a18a170f791769691fe905cfe76d103b7a345e87
erdgeist <erdgeist@erdgeist.org>
2026-07-14 00:11:45 +0200

    Re-add language selector, but fix the mis-nomer for english pages

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,4 +1,13 @@
 module AdminHelper
+  def language_selector
+    case I18n.locale
+    when :de
+      link_to raw('<span class="inactive">English</span>'), url_for(params.permit(:locale, :page_path).to_h.merge('locale' => 'en'))
+    when :en
+      link_to raw('<span class="inactive">Deutsch</span>'), url_for(params.permit(:locale, :page_path).to_h.merge('locale' => 'de'))
+    end
+  end
+
   def mtime_busted_path(path)
     file = Rails.public_path.join(path.sub(%r{\A/}, ""))
     raise "Static asset not found for cache-busting: #{path} (looked for #{file})" unless File.exist?(file)
