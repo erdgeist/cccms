833c59abf109943e857f64e526f6434e095242d6
erdgeist <erdgeist@erdgeist.org>
2026-08-02 18:59:03 +0200

    Give the session cookie a lifetime so it survives app switches

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Cccms::Application.config.session_store :cookie_store, :key => '_cccms_session_v2'
+Cccms::Application.config.session_store :cookie_store,
+  :key          => '_cccms_session_v2',
+  :expire_after => 7.days
